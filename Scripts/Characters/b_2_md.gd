77791c630ac3b2598d82e09ccef0814a63b9459f
Noah Timmons <ntimmonswork@gmail.com>
2025-04-21 13:10:03 -0500

    Added more character functionality

--- Scripts/Characters/b_2_md.gd
+++ Scripts/Characters/b_2_md.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
+
+
 var direction: Vector2
 var SPEED = 8000.0
 const JUMP_VELOCITY = -2000.0
 
 var inputs: Array = [5]
 var input_movement: int
+
+@export_group("players")
+@export var enemy: CharacterBody2D
+
 
 
 func input_calc():
@@ -16,39 +22,54 @@ func input_calc():
 func get_movement_input():
 	if Input.is_action_pressed("Crouch") and Input.is_action_pressed("Move_left"):
 		input_movement = 1
-	
 	elif Input.is_action_pressed("Crouch") and Input.is_action_pressed("Move_right"):
 		input_movement = 3
-	
 	elif Input.is_action_pressed("Jump") and Input.is_action_pressed("Move_left"):
 		input_movement = 7
-	
 	elif Input.is_action_pressed("Jump") and Input.is_action_pressed("Move_right"):
 		input_movement = 9
-	
 	elif Input.is_action_pressed("Crouch"):
 		input_movement = 2
-	
 	elif Input.is_action_pressed("Jump"):
 		input_movement = 8
-	
 	elif Input.is_action_pressed("Move_left"):
 		input_movement = 4
-		
 	elif Input.is_action_pressed("Move_right"):
 		input_movement = 6
-		
 	else:
 		input_movement = 5
+
+
 
 func _physics_process(delta: float) -> void:
 	get_movement_input()
 	input_calc()
+	
+
+func check_for_enemy():
+	if get_node_or_null("Node/Player2") == null:
+		return false
+	else:
+		return true
+
+func check_enemy_position(enemy: CharacterBody2D):
+	#if enemy is to the right
+	if enemy.global_position.x > self.global_position.x:
+		$Player_animations.flip_h = true
+		change_punch_position_right()
+	
+	#if to the left
+	elif enemy.global_position.x < self.global_position.x:
+		$Player_animations.flip_h = false
+		change_punch_position_left()
+func change_punch_position_left():
+	$Attacks/punch.position.x = -81.75
+func change_punch_position_right():
+	$Attacks/punch.position.x = 81.75
 
 func _on_idle_state_entered() -> void:
 	$Player_animations.play("Idle")
 	print("idle")
-
 func _on_idle_state_processing(delta: float) -> void:
 	#if walking
 	if Input.is_action_pressed("Movement"):
@@ -67,17 +88,19 @@ func _on_idle_state_processing(delta: float) -> void:
 			return
 		else:
 			$StateChart.send_event("to_crouch")
-
+	#if punching
+	if Input.is_action_just_pressed("Punch"):
+		$StateChart.send_event("to_punch")
 func _on_idle_state_physics_processing(delta: float) -> void:
-	pass
+	#if enemy is there
+	if check_for_enemy():
+		check_enemy_position(enemy)
 
 
 
 func _on_walk_state_entered() -> void:
 	$Player_animations.play("Walk")
 	print("walk")
-
-
 func _on_walk_state_processing(delta: float) -> void:
 	#if not moving
 	if velocity == Vector2(0, 0):
@@ -85,12 +108,14 @@ func _on_walk_state_processing(delta: float) -> void:
 	#if jumping
 	if abs(velocity.y) > 0:
 		$StateChart.send_event("to_jump")
-		
 	#if crouching
 	if Input.is_action_pressed("Crouch"):
 		$StateChart.send_event("to_crouch")
-	
-
+	if Input.is_action_just_pressed("Dash") and inputs[-1] == 6:
+		$StateChart.send_event("to_dash")
+	#if punching
+	if Input.is_action_just_pressed("Punch"):
+		$StateChart.send_event("to_punch")
 func _on_walk_state_physics_processing(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
@@ -102,7 +127,7 @@ func _on_walk_state_physics_processing(delta: float) -> void:
 		velocity.x = direction * SPEED * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	check_enemy_position(enemy)
 	move_and_slide()
 
 
@@ -145,6 +170,7 @@ func _on_double_jump_state_entered() -> void:
 	if inputs[-1] == 9:
 		direction.x = 1
 	velocity.y = JUMP_VELOCITY
+	check_enemy_position(enemy)
 func _on_double_jump_state_processing(delta: float) -> void:
 	if self.is_on_floor():
 		#if not moving on floor
@@ -161,9 +187,38 @@ func _on_double_jump_state_physics_processing(delta: float) -> void:
 func _on_crouch_state_entered() -> void:
 	print("crouch")
 func _on_crouch_state_processing(delta: float) -> void:
+	#if no longer crouching
 	if !Input.is_action_pressed("Crouch"):
 		$StateChart.send_event("to_idle")
+	#if attempting to jump while crouching
 	if Input.is_action_pressed("Jump"):
 		$StateChart.send_event("to_idle")
 func _on_crouch_state_physics_processing(delta: float) -> void:
-	pass # Replace with function body.
+	velocity = Vector2.ZERO
+	check_enemy_position(enemy)
+	move_and_slide()
+
+
+func _on_step_dash_state_entered() -> void:
+	print("Step Dash!")
+	direction.x = 10
+	$timers/dash.start()
+func _on_step_dash_state_processing(delta: float) -> void:
+	pass
+func _on_step_dash_state_physics_processing(delta: float) -> void:
+	velocity.x = direction.x * SPEED * delta
+	move_and_slide()
+func _on_timer_timeout() -> void:
+	print("timeout")
+	direction.x = 0
+	$StateChart.send_event("to_idle")
+
+
+
+func _on_punch_state_entered() -> void:
+	#play punch animation
+	print("punch")
+	$Attacks_ani.play("punch")
+	$timers/punch/punch.start()
+func _on_punch_timeout() -> void:
+	$StateChart.send_event("to_idle")
